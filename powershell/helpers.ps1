e99e5126b433c6fcff81853b58465e4e451d1d50
Eric Nelson <e82.eric@gmail.com>
2023-07-28 17:30:42 -0700

    Extracted text helper functions from powershell profile and moved cal to powershell directory

---
+++ powershell/helpers.ps1
@@ -0,0 +1,71 @@
+function AddToMachinePath($pathToAdd)
+{
+  $oldPath = [Environment]::GetEnvironmentVariable("PATH", "MACHINE")
+  $newPath = "$($oldPath);$($pathToAdd)"
+
+  Write-Host "New path is going to be:"
+  Write-Host ""
+  Write-Host $newPath
+  Write-Host ""
+
+  $confirmation = Read-Host "Confirm [y/n]"
+  if ($confirmation -eq 'y')
+  {
+    [Environment]::SetEnvironmentVariable("PATH", $newPath, "MACHINE")
+  }
+}
+
+function FormatXml
+{
+  param($string)
+    $Indent = 4
+    $xml = [xml]($string)
+    $StringWriter = New-Object System.IO.StringWriter
+    $XmlWriter = New-Object System.XMl.XmlTextWriter $StringWriter
+    $xmlWriter.Formatting = "indented"
+    $xmlWriter.Indentation = $Indent
+    $xml.WriteContentTo($XmlWriter)
+    $XmlWriter.Flush()
+    $StringWriter.Flush()
+    Write-Output $StringWriter.ToString()
+}
+
+function FormatJson
+{
+  param($string)
+    $result = $string | ConvertFrom-Json | ConvertTo-Json -depth 100
+    $result
+}
+
+function TryJson
+{
+  param($string)
+    $first = $string.IndexOf('{')
+    $last = $string.LastIndexOf('}')
+    $string.SubString(0, $first)
+    $result = FormatJson $string.SubString($first, $last - $first + 1)
+    $result
+}
+
+function OneLineJson
+{
+  param($string)
+    $result = $string | ConvertFrom-Json | ConvertTo-Json -depth 100 -Compress
+    $result
+}
+
+function Base64Encode
+{
+  param($string)
+    $bytes = [System.Text.Encoding]::UTF8.GetBytes($string)
+    $result = [Convert]::ToBase64String($bytes)
+    $result
+}
+
+function Base64Decode
+{
+  param($string)
+    $bytes = [Convert]::FromBase64String($string)
+    $result = [Text.Encoding]::UTF8.GetString($bytes)
+    $result
+}
