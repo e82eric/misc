1ea97334c85469557916b84493bf24f06695ac06
Eric Nelson <e82.eric@gmail.com>
2023-09-25 18:16:30 -0700

    Updated fuzzy commands to take a search string parameter

--- cmds/fdir.cmd
+++ cmds/fdir.cmd
@@ -2,15 +2,26 @@
 setlocal EnableDelayedExpansion
 @SET _dest=
 @SET _rootDir=
+@SET _queryString=
 
-IF "%~1" == "" (
-  SET _rootDir=%USERPROFILE%
+IF "%~2" == "" (
+  IF "%~1" == "" (
+    SET _rootDir=%USERPROFILE%
+  ) ELSE (
+    if exist %~1\ (
+      SET _rootDir=%~1
+    ) else (
+      SET _queryString=%~1
+      SET _rootDir=%USERPROFILE%
+    )
+  )
 ) ELSE (
   SET _rootDir=%~1
+  SET _queryString=%~2
 )
 
 FOR /F "delims=" %%I IN (
-'dir /s /b /a:d "!_rootDir!" ^| fzf -e --layout reverse --info inline --bind="ctrl-y:execute(echo {} ^| clip)" --preview="tree /f /a {}" --preview-window=up'
+'dir /s /b /a:d "!_rootDir!" ^| fzf --query="!_queryString!" -e --layout reverse --info inline --bind="ctrl-y:execute(echo {} ^| clip)" --preview="tree /f /a {}" --preview-window=up'
 ) DO @SET "_dest=%%I"
 
 IF "!_dest!" == "" EXIT /B
--- cmds/ff.cmd
+++ cmds/ff.cmd
@@ -2,15 +2,26 @@
 setlocal EnableDelayedExpansion
 @SET _dest=
 @SET _rootDir=
+@SET _queryString=
 
-IF "%~1" == "" (
-  SET _rootDir=%USERPROFILE%
+IF "%~2" == "" (
+  IF "%~1" == "" (
+    SET _rootDir=%USERPROFILE%
+  ) ELSE (
+    if exist %~1\ (
+      SET _rootDir=%~1
+    ) else (
+      SET _queryString=%~1
+      SET _rootDir=%USERPROFILE%
+    )
+  )
 ) ELSE (
   SET _rootDir=%~1
+  SET _queryString=%~2
 )
 
 FOR /F "delims=" %%I IN (
-'dir /s /b /a-d "!_rootDir!" ^| fzf -e --layout reverse --info inline --bind="ctrl-y:execute(echo {} ^| clip)" --preview-window=up --preview="bat.exe --style=numbers --theme=gruvbox-dark --color=always {}"'
+'dir /s /b /a-d "!_rootDir!" ^| fzf --query="!_queryString!" -e --layout reverse --info inline --bind="ctrl-y:execute(echo {} ^| clip)" --preview-window=up --preview="bat.exe --style=numbers --theme=gruvbox-dark --color=always {}"'
 ) DO @SET "_dest=%%I"
 
 IF "!_dest!" == "" EXIT /B
